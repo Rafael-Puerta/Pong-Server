c8fc1d2d497dd6cc4bb25abb4da5a54895e10ae2
Rafael-Puerta <rafapc1992@gmail.com>
2023-03-21 18:58:43 +0100

    Cleaned example

--- app.js
+++ app.js
@@ -15,53 +15,10 @@ const app = express()
 // Set port number
 const port = process.env.PORT || 3000
 
-// Publish static files from 'public' folder
-app.use(express.static('public'))
-
 // Activate HTTP server
 const httpServer = app.listen(port, appListen)
 function appListen () {
   console.log(`Listening for HTTP queries on: http://localhost:${port}`)
-}
-
-// Set URL rout for POST queries
-app.post('/dades', getDades)
-async function getDades (req, res) {
-  let receivedPOST = await post.getPostObject(req)
-  let result = { status: "KO", result: "Unkown type" }
-
-  var textFile = await fs.readFile("./public/consoles/consoles-list.json", { encoding: 'utf8'})
-  var objConsolesList = JSON.parse(textFile)
-
-  if (receivedPOST) {
-    if (receivedPOST.type == "consola") {
-      var objFilteredList = objConsolesList.filter((obj) => { return obj.name == receivedPOST.name })
-      await wait(1500)
-      if (objFilteredList.length > 0) {
-        result = { status: "OK", result: objFilteredList[0] }
-      }
-    }
-    if (receivedPOST.type == "marques") {
-      var objBrandsList = objConsolesList.map((obj) => { return obj.brand })
-      await wait(1500)
-      let senseDuplicats = [...new Set(objBrandsList)]
-      result = { status: "OK", result: senseDuplicats.sort() } 
-    }
-    if (receivedPOST.type == "marca") {
-      var objBrandConsolesList = objConsolesList.filter ((obj) => { return obj.brand == receivedPOST.name })
-      await wait(1500)
-      // Ordena les consoles per nom de model
-      objBrandConsolesList.sort((a,b) => { 
-          var textA = a.name.toUpperCase();
-          var textB = b.name.toUpperCase();
-          return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-      })
-      result = { status: "OK", result: objBrandConsolesList } 
-    }
-  }
-
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify(result))
 }
 
 // Run WebSocket server
